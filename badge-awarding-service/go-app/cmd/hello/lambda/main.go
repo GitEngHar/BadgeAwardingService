2838c8cd685b71195501ee36de860782272ce5e4
GitEngHar <harukii1999@gmail.com>
2025-07-14 07:52:06 +0900

    lambda関数を追加

---
+++ badge-awarding-service/go-app/cmd/hello/lambda/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var greeting string
+	proxyPath := request.PathParameters["proxy"]
+	sourceIP := request.RequestContext.Identity.SourceIP
+
+	if sourceIP == "" {
+		greeting = "Hello, world!\n"
+	} else {
+		greeting = fmt.Sprintf("Hello, %s!\n", sourceIP)
+	}
+
+	if proxyPath != "" {
+		greeting = fmt.Sprintf("%s\n path: %s!\n", greeting, proxyPath)
+	} else {
+		greeting = fmt.Sprintf("%s\n path: %s!\n", greeting, "path empty")
+	}
+
+	return events.APIGatewayProxyResponse{
+		Body:       greeting,
+		StatusCode: 200,
+	}, nil
+}
+
+func main() {
+	lambda.Start(handler)
+}
