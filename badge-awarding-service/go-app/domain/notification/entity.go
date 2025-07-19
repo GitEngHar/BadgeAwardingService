130aaccd4da79875718038e976f7b043557f4f77
GitEngHar <harukii1999@gmail.com>
2025-07-19 17:23:32 +0900

    badge のインフラ層を追加

--- badge-awarding-service/go-app/domain/notification/entity.go
+++ badge-awarding-service/go-app/domain/notification/entity.go
@@ -5,20 +5,6 @@ import (
 	"time"
 )
 
-type MailMessage struct {
-	To      string
-	Subject string
-	Body    string
-}
-
-func NewMailMessage(to, subject, body string) *MailMessage {
-	return &MailMessage{
-		To:      to,
-		Subject: subject,
-		Body:    body,
-	}
-}
-
 type MessagePublisher interface {
 	Publish(ctx context.Context, message MailMessage) error
 }
@@ -39,17 +25,4 @@ func NewSubscriber(id, endpoint, protocol string, subscribed bool, createdAt tim
 		Subscribed: subscribed,
 		CreatedAt:  createdAt,
 	}
-}
-
-type SubscriberService interface {
-	Subscribe(ctx context.Context, subscription Subscriber) error
-	UnSubscribe(ctx context.Context, subscription Subscriber) error
-	ListSubscriber(ctx context.Context) ([]Subscriber, error)
-}
-
-type SubscriberRepository interface {
-	Save(ctx context.Context, s *Subscriber) error
-	Delete(ctx context.Context, id string) error
-	FindAll(ctx context.Context) ([]*Subscriber, error)
-	FindByID(ctx context.Context, id string) (*Subscriber, error)
 }
--- badge-awarding-service/go-app/infra/db/dynamo/user_repository.go
+++ badge-awarding-service/go-app/infra/db/dynamo/user_repository.go
@@ -1,5 +1,53 @@
 package dynamo
 
-type UserRepository struct {
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"hello-world/domain/management"
+)
+
+const (
+	userTableName = "users"
+)
+
+type UserRepositorySt struct {
 	config Config
+}
+
+func NewUserRepository(ctx context.Context) management.UserRepository {
+	dbConfig, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		panic("unable to load SDK config, " + err.Error())
+	}
+	client := dynamodb.NewFromConfig(dbConfig)
+	conf := Config{
+		tableName: userTableName,
+		client:    client,
+	}
+	return UserRepositorySt{
+		config: conf,
+	}
+}
+
+func (u UserRepositorySt) Upsert(ctx context.Context, user management.User) error {
+	return nil
+}
+
+func (u UserRepositorySt) Create(ctx context.Context, user management.User) error {
+	return nil
+}
+
+func (u UserRepositorySt) GetByID(ctx context.Context, id string) (management.User, error) {
+	// mockで作る
+	return management.User{}, nil
+}
+
+func (u UserRepositorySt) Delete(ctx context.Context, id string) error {
+	return nil
+}
+
+// CreateTable テーブルを作成する
+func (u UserRepositorySt) CreateTable(ctx context.Context) error {
+	return nil
 }
