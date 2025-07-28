314570105dbb349587477a62527ea7fa3f059d9b
GitEngHar <harukii1999@gmail.com>
2025-07-29 07:53:24 +0900

    badgeの機能を実装

--- badge-awarding-service/go-app/infra/s3/repository.go
+++ badge-awarding-service/go-app/infra/s3/repository.go
@@ -1,18 +1,18 @@
 package s3
 
 import (
-	"context"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"hello-world/domain/management"
+	"net/url"
 )
 
 type BadgeImageRepository struct {
 	config Config
 }
 
-func NewConfig(ctx context.Context, bucketName, bucketKey string) *Config {
+func NewConfig(bucketName, bucketKey string) *Config {
 	// セッション作成
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String("ap-northeast-1"),
@@ -33,6 +33,23 @@ func NewBadgeImageRepository(config Config) management.BadgeImgRepository {
 	return BadgeImageRepository{config: config}
 }
 
-func (b BadgeImageRepository) DownloadBadge(ctx context.Context) (*management.Badge, error) {
-	return nil, nil
+// DownloadBadge S3から画像を取得してオブジェクトに画像情報を格納して渡す。画像を配信する際に利用する
+func (b BadgeImageRepository) DownloadBadge() (*management.BadgeImg, error) {
+	// Badgeの画像を取得
+	result, err := b.config.client.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(b.config.bucketName),
+		Key:    aws.String(b.config.bucketKey),
+	})
+	if err != nil {
+		return nil, err
+	}
+	defer result.Body.Close()
+	img, err := management.S3BodyConvertToImage(result)
+	if err != nil {
+		return nil, err
+	}
+	// TODO: ユースケースがはっきりしたら精査する
+	imgUrl := url.URL{}
+	badgeImg, err := management.NewBadgeImg(img, imgUrl)
+	return badgeImg, err
 }
